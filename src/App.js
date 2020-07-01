7896e53d1f4d9bc00d399197e11bf0f02ed3f92c
Ibitoye Ayanfeoluwa <ibitoyeayanfeoluwa@gmail.com>
2020-07-01 11:26:34 +0100

    Begin

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import { Router } from '@reach/router';
+
+import Home from './home/home';
+import Comics from './comics/comics';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Router>
+      <Home path='/' />
+      <Comics path='/comics' />
+    </Router>
   );
 }
 
---
+++ src/comics/comics.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+function Comics() {
+
+  return <h1>Comics</h1>
+}
+
+export default Comics;
