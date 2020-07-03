31877954075d8ee6b3053d6c7d82cea8e836a424
Ibitoye Ayanfeoluwa <ibitoyeayanfeoluwa@gmail.com>
2020-07-03 17:15:19 +0100

    Add cors and caching to express server

--- server.mjs
+++ server.mjs
@@ -1,13 +1,52 @@
 import express from 'express';
+import cors from 'cors';
 import path, { dirname } from 'path';
 import { fileURLToPath } from 'url';
+import ExpressCache from 'express-cache-middleware';
+import cacheManager from 'cache-manager';
+import redisStore from 'cache-manager-redis-store';
 
-import { PORT } from './config/constants.mjs';
+import { PORT, CORS_HOSTS } from './config/constants.mjs';
 import api from './api/api.mjs';
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
+
+let caching;
+
+if (process.env.NODE_ENV === "development") {
+  caching = cacheManager.caching({
+    store: 'memory', max: 10000, ttl: 3600
+  })
+} else {
+  caching = cacheManager.caching({
+    store: redisStore,
+    url: process.env.REDIS_URL,
+    ttl: 3600,
+  });
+}
+
+const hosts = CORS_HOSTS.split(', ');
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (!origin) return callback(null, true);
+
+    if(hosts.indexOf(origin) === -1){
+      var msg = 'The CORS policy for this site does not ' +
+                'allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+
+    return callback(null, true);
+  },
+}
+
+app.use(cors(corsOptions));
+
+const cacheMiddleware = new ExpressCache(caching);
+
+cacheMiddleware.attach(app);
 
 app.use(express.static(
   path.join(__dirname, '/app')
