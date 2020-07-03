ca83078badc92b83d33d30c88a89a5d06da69f14
Ibitoye Ayanfeoluwa <ibitoyeayanfeoluwa@gmail.com>
2020-07-03 18:02:55 +0100

    I keep making dumb mistakes, lol

--- config/constants.mjs
+++ config/constants.mjs
@@ -1,5 +1,7 @@
+const dev = process.env.NODE_ENV === "development";
+
 export const {
-  REACT_APP_API_PORT: PORT = (process.env.NODE_ENV ? 8080 : process.env.PORT),
+  REACT_APP_API_PORT: PORT = (dev ? 8080 : process.env.PORT),
   MARVEL_API_PUBLIC_KEY: PUBLIC_KEY,
   MARVEL_API_PRIVATE_KEY: PRIVATE_KEY,
   CORS_HOSTS = "",
