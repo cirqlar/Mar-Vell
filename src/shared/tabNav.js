4ba9abb2c6c33b744cea35fe0b5ac1bb551c7c5c
Ibitoye Ayanfeoluwa <ibitoyeayanfeoluwa@gmail.com>
2020-07-04 14:28:16 +0100

    complete tabNav and home hero styling

--- src/shared/tabNav.js
+++ src/shared/tabNav.js
@@ -1,15 +1,41 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { Link, useLocation } from '@reach/router';
 import cn from 'classnames';
 
 import styles from './styles/tabNav.module.css';
 
+let scrollTop;
+let width;
+let top;
+
 function TabNav() {
+  const nav = useRef();
+  const [fixed, setFixed] = useState(false);
   const location = useLocation();
 
+  window.addEventListener('scroll', (ev) => {
+    scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    width = window.innerWidth || document.documentElement.clientWidth;
+  })
+
+  useEffect(() => {
+    top = nav.current.getBoundingClientRect().top;
+    const interval = setInterval(() => {
+      if (width >= 839 && scrollTop && scrollTop > top) {
+        setFixed(true);
+      } else {
+        setFixed(false);
+      }
+    }, 10);
+    return () => clearInterval(interval);
+  }, []);
+
   return (
-    <nav className={styles.tabs}>
-      <ul className={styles.tabList}>
+    <nav className={styles.tabs} ref={nav}>
+      <ul className={cn({
+        [styles.tabList]: !fixed,
+        [styles.tabListFixed]: fixed,
+      })}>
         <li className={cn({
           [styles.tabItem]: true,
           [styles.tabItemActive]: (location.pathname === '/'),
