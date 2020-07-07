fb672c1a42cb1d22fc9ee655ba8c85ef3e5bf300
Ibitoye Ayanfeoluwa <ibitoyeayanfeoluwa@gmail.com>
2020-07-07 18:19:54 +0100

    Redirect to https

--- server.mjs
+++ server.mjs
@@ -14,6 +14,16 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 
+app.use((req, res, next) => {
+  if (process.env.NODE_ENV === 'production') {
+    if (req.headers['x-forwarded-proto'] !== 'https')
+      return res.redirect('https://' + req.headers.host + req.url);
+    else
+      return next();
+  } else
+    return next();
+});
+
 let caching;
 
 if (process.env.NODE_ENV === "development") {
